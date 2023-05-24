e719d0b9b9941b0d06d983cb897c42f948f512dd
Илья <tankolom-x@yandex.ru>
2023-05-24 19:55:15 +0500

    new interface and the ability to select a directory

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ import datetime as dt
 import os
 from git import rmtree
 from tkinter import Tk
-from tkinter.filedialog import askopenfilename
+from tkinter.filedialog import askopenfilename, askdirectory
 
 Tk().withdraw()
 path_to_image = askopenfilename(title='Select an image')
+directory = askdirectory(title='Select a directory')
 year = int(input('Year for commits:\n'))
 
 size = (51, 7)
@@ -33,12 +34,17 @@ def get_commits(image_path):
 
     result = im.quantize(colors_amount)
 
+    if len(result.getcolors()) == 1 and colors_amount == 5:
+        print('Cannot transform white image')
+        exit()
+
+    max_color = len(result.getcolors()) - 1
     pixels = result.load()
     colors = list()
     for i in range(size[0]):
         line = list()
         for j in range(size[1]):
-            line.append(pixels[i, j])
+            line.append(4 if pixels[i, j] == max_color else pixels[i, j])
         colors.append(line)
 
     return colors
@@ -58,6 +64,10 @@ def first_date(year):
     return date
 
 
+pixels_colors = get_commits(path_to_image)
+commit_date = first_date(year)
+
+os.chdir(directory)
 if os.path.isfile('commits_file.txt'):
     os.remove('commits_file.txt')
 if os.path.isdir('.git'):
@@ -67,11 +77,7 @@ os.system('type nul > commits_file.txt')
 os.system('git init')
 os.system('git add commits_file.txt')
 
-commit_date = first_date(year)
-
-pixels_colors = get_commits(path_to_image)
 k = 1 if colors_amount == 4 else 0
-
 for row in range(size[1]):
     for col in range(size[0]):
         for _ in range(pixels_colors[col][row] + k):
