695532a50dafc9a9b307ad1ec90770df6b21f017
Илья <tankolom-x@yandex.ru>
2023-06-06 20:52:58 +0500

    feat(main): draw image from the date

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from random import randint
 
 SIZE = (51, 7)
 colors_amount = 4
+commits = list()
 
 
 def modal_image_select():
@@ -33,27 +34,20 @@ def modal_directory_select():
     return directory
 
 
-def year_select():
-    year = int(input('Year for commits:\n'))
-    if not year:
-        exit()
-    return year
+def date_select(beginning=True):
+    if beginning:
+        message = 'Please specify the beginning date in YYYY-MM-DD format \n'
+        year, month, day = input(message).split('-')
+        date = dt.datetime(day=int(day), month=int(month), year=int(year), hour=12)
+    else:
+        message = 'Please specify the ending date in YYYY-MM-DD format \n'
+        year, month, day = input(message).split('-')
+        date = dt.datetime(day=int(day), month=int(month), year=int(year), hour=12)
+    return date
 
 
-def date_select():
-    message = 'Please specify the beginning date in YYYY-MM-DD format \n'
-    year, month, day = input(message).split('-')
-    beginning = dt.datetime(day=int(day), month=int(month), year=int(year), hour=12)
-
-    message = 'Please specify the ending date in YYYY-MM-DD format \n'
-    year, month, day = input(message).split('-')
-    ending = dt.datetime(day=int(day), month=int(month), year=int(year), hour=12)
-
-    return beginning, ending
-
-
-def get_commits(image_path):
-    global colors_amount
+def image_conversion(image_path):
+    global colors_amount, commits
 
     im = Image.open(image_path)
     if im.size != SIZE:
@@ -76,33 +70,35 @@ def get_commits(image_path):
         print('Cannot transform white image')
         exit()
 
+    delta = 1 if colors_amount == 4 else 0
     max_color = len(result.getcolors()) - 1
     pixels = result.load()
-    colors = list()
     for i in range(SIZE[0]):
         line = list()
         for j in range(SIZE[1]):
-            line.append(4 if pixels[i, j] == max_color else pixels[i, j])
-        colors.append(line)
-
-    return colors
+            line.append(4 if pixels[i, j] == max_color else pixels[i, j] + delta)
+        commits.append(line)
 
 
-def first_date(year):
-    day = 1
-    month = 1
-    if dt.date(year, month=month, day=day).weekday() == 6:
-        date = dt.datetime(year, month=1, day=8, hour=12)
-    else:
-        while dt.date(year, month=month, day=day).weekday() != 6:
-            day += 1
+def random_conversion(beginning, ending, minimal, maximal):
+    empty_amount = (beginning.weekday() + 1) % 7
+
+    line = list()
+    for i in range((ending - beginning).days + 1 + empty_amount):
+        if len(line) == 7:
+            commits.append(line)
+            line = list()
+        if i in range(empty_amount):
+            line.append(0)
         else:
-            date = dt.datetime(year, month=month, day=day, hour=12)
+            line.append(randint(minimal, maximal))
+    if line:
+        while len(line) < 7:
+            line.append(0)
+        commits.append(line)
 
-    return date
 
-
-def prepare_for_commit(directory_path):
+def prepare_to_commits(directory_path):
     os.chdir(directory_path)
     if os.path.isfile('commits_file.txt'):
         os.remove('commits_file.txt')
@@ -111,20 +107,20 @@ def prepare_for_commit(directory_path):
     os.system('git init')
 
 
-def make_commits(commit_date, commits):
-    k = 1 if colors_amount == 4 else 0
-    for row in range(SIZE[1]):
-        for col in range(SIZE[0]):
-            for i in range(commits[col][row] + k):
+def make_commits(beginning):
+    commit_date = beginning
+    for week in range(len(commits)):
+        for day in range((beginning.weekday() + 1) % 7 if week == 0 else 0, len(commits[0])):
+            for commit in range(commits[week][day]):
                 f = open('commits_file.txt', 'a')
                 f.write('0')
                 f.close()
                 os.system('git add commits_file.txt')
-                command = f'git commit -m \"{commit_date.date()} {i + 1}\" --no-edit --date=\"{commit_date}\"'
+                command = f'git commit -m \"{commit_date.date()} {commit + 1}\" --no-edit --date=\"{commit_date}\"'
                 os.system(command)
-            commit_date += dt.timedelta(days=7)
-        commit_date -= dt.timedelta(days=7 * 51 - 1)
+            commit_date += dt.timedelta(days=1)
     print('Commits were generated. You can push them to your empty GitHub repository')
+
 
 def menu():
     while True:
@@ -139,39 +135,32 @@ def menu():
             print('Choice is incorrect')
 
 
-index = menu()
-if index == 1:
-    path_to_image = modal_image_select()
+def main():
+    index = menu()
 
-    directory = modal_directory_select()
-    year = year_select()
+    if index == 1:
+        path_to_image = modal_image_select()
+        directory = modal_directory_select()
+        beginning = date_select()
 
-    prepare_for_commit(directory)
-    commits = get_commits(path_to_image)
-    commit_date = first_date(year)
-    make_commits(commit_date, commits)
+        image_conversion(path_to_image)
 
-elif index == 2:
-    directory = modal_directory_select()
-    beginning, ending = date_select()
-    minimal = int(input('Specify the minimal amount of commits \n'))
-    maximal = int(input('Specify the maximal amount of commits \n'))
+    elif index == 2:
+        directory = modal_directory_select()
+        beginning = date_select()
+        ending = date_select(beginning=False)
+        minimal = int(input('Specify the minimal amount of commits \n'))
+        maximal = int(input('Specify the maximal amount of commits \n'))
 
-    prepare_for_commit(directory)
+        if beginning == ending:
+            print('Dates are the same')
+            exit()
 
-    if beginning == ending:
-        print('Dates are the same')
-        exit()
+        random_conversion(beginning, ending, minimal, maximal)
 
-    commit_date = beginning
-    for _ in range((ending - beginning).days + 1):
-        for i in range(randint(minimal, maximal)):
-            f = open('commits_file.txt', 'a')
-            f.write('0')
-            f.close()
-            os.system('git add commits_file.txt')
-            command = f'git commit -m \"{commit_date.date()} {i + 1}\" --no-edit --date=\"{commit_date}\"'
-            os.system(command)
-        commit_date += dt.timedelta(days=1)
-    print('Commits were generated. You can push them to your empty GitHub repository')
+    prepare_to_commits(directory)
+    make_commits(beginning)
 
+
+if __name__ == '__main__':
+    main()
