cf4b94ce83299f3c00b8249433941591e63cce32
Илья <tankolom-x@yandex.ru>
2023-11-11 12:39:41 +0500

    feat(ImageConversionWindow): start making commits with the click of the button

--- src/CommitDraw/ImageConversionWindow.py
+++ src/CommitDraw/ImageConversionWindow.py
@@ -1,9 +1,11 @@
 from PyQt5.QtWidgets import QMainWindow, QLineEdit, QCalendarWidget, QLabel, QPushButton
 from PyQt5.QtWidgets import QFileDialog, QMessageBox
 from PyQt5.QtGui import QPixmap
+from PyQt5.QtCore import QTimer
 from PyQt5 import uic
 import datetime as dt
 from ImageConverter import ImageConverter
+from Commiter import Commiter
 
 
 class ImageConversionWindow(QMainWindow):
@@ -21,18 +23,24 @@ class ImageConversionWindow(QMainWindow):
         self.author_lineEdit = self.findChild(QLineEdit, 'author_lineEdit')
         self.email_lineEdit = self.findChild(QLineEdit, 'email_lineEdit')
         self.calendarWidget = self.findChild(QCalendarWidget, 'calendarWidget')
+        self.in_progress_label = self.findChild(QLabel, 'label')
+        self.in_progress_label.setVisible(False)
 
         self.previous_settings_btn = self.findChild(QPushButton, 'previous_settings_btn')
         self.start_btn = self.findChild(QPushButton, 'start_btn')
 
         self.image_label.mouseReleaseEvent = self.image_select
         self.directory_label.mouseReleaseEvent = self.directory_select
-
+        self.start_btn.clicked.connect(self.on_click_start)
         self.calendarWidget.clicked['QDate'].connect(self.date_select)
 
         self.date = None
         self.image_path = None
         self.directory = None
+        self.author = None
+        self.email = None
+        self.commits = None
+
 
     def set_image(self, filename, label_name):
         pixmap = QPixmap(filename).scaled(459, 63)
@@ -52,6 +60,8 @@ class ImageConversionWindow(QMainWindow):
             result = image_converter.make_a_result_image(self.date, self.image_path)
             if not result:
                 return 'White image'
+
+            self.commits = image_converter.commits
             self.set_image('../../samples/result.png', 'image_result_label')
 
     def image_select(self, event):
@@ -81,3 +91,20 @@ class ImageConversionWindow(QMainWindow):
         if directory:
             self.directory = directory
             self.directory_label.setText(directory)
+
+    def start(self):
+        self.in_progress_label.setVisible(True)
+        QTimer.singleShot(2, self.commiting)
+
+    def commiting(self):
+        commiter = Commiter(self.commits, self.directory, self.date, self.author, self.email)
+        commiter.prepare_to_commits()
+        commiter.make_commits()
+        self.in_progress_label.setVisible(False)
+
+    def on_click_start(self):
+        if self.commits and self.date and self.directory:
+            self.author = self.author_lineEdit.text()
+            self.email = self.email_lineEdit.text()
+
+            self.start()
